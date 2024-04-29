5c9365e31565568c5c17935da3c0b31ff294309a
Kevin <kevin.orsho@hotmail.fr>
2024-04-29 10:47:21 +0200

    etape 6 page 404

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 function Error(){
     return(
-        <div>
-            <h1>Cette route n'éxiste pas</h1>
+        <div className="error">
+            <h1 className="error__h1">404</h1>
+            <p className="error__p">Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="error__link">Retourner sur la page d'accueil</Link>
         </div>
     )    
 }
